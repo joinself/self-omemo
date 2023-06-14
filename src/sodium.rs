55817aa42158f327bd1e2ea0724b421f3c5dcecb
Tom Bevan <tom@joinself.com>
2023-06-14 23:41:26 +0400

    use word size independent types (#27)

--- src/sodium.rs
+++ src/sodium.rs
@@ -1,4 +1,4 @@
-use libc::c_void;
+use libc::{c_char, c_void, size_t};
 use sodium_sys::*;
 
 pub const self_base64_VARIANT_URLSAFE: u32 = sodium_base64_VARIANT_URLSAFE;
@@ -16,12 +16,12 @@ pub const self_crypto_aead_xchacha20poly1305_ietf_KEYBYTES: u32 =
 #[no_mangle]
 pub unsafe extern "C" fn self_base642bin(
     bin: *mut u8,
-    bin_maxlen: u64,
-    b64: *const i8,
-    b64_len: u64,
-    ignore: *const i8,
-    bin_len: *mut u64,
-    b64_end: *mut *const i8,
+    bin_maxlen: size_t,
+    b64: *const c_char,
+    b64_len: size_t,
+    ignore: *const c_char,
+    bin_len: *mut size_t,
+    b64_end: *mut *const c_char,
     variant: i32,
 ) -> i32 {
     sodium_base642bin(
@@ -31,17 +31,17 @@ pub unsafe extern "C" fn self_base642bin(
 
 #[no_mangle]
 pub unsafe extern "C" fn self_bin2base64(
-    b64: *mut i8,
-    b64_maxlen: u64,
+    b64: *mut c_char,
+    b64_maxlen: size_t,
     bin: *const u8,
-    bin_len: u64,
+    bin_len: size_t,
     variant: i32,
-) -> *mut i8 {
+) -> *mut c_char {
     sodium_bin2base64(b64, b64_maxlen, bin, bin_len, variant)
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn self_base64_ENCODED_LEN(bin_len: u64, variant: i32) -> u64 {
+pub unsafe extern "C" fn self_base64_ENCODED_LEN(bin_len: size_t, variant: i32) -> size_t {
     sodium_base64_encoded_len(bin_len, variant)
 }
 
@@ -97,11 +97,25 @@ pub unsafe extern "C" fn self_crypto_sign_ed25519_sk_to_curve25519(
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn self_crypto_sign_publickeybytes() -> u64 {
+pub unsafe extern "C" fn self_crypto_sign_publickeybytes() -> size_t {
     crypto_sign_publickeybytes()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn self_randombytes_buf(buf: *mut c_void, size: u64) {
+pub unsafe extern "C" fn self_self_crypto_sign_secretkeybytes() -> size_t {
+    crypto_sign_secretkeybytes()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn self_crypto_sign_seed_keypair(
+    pk: *mut u8,
+    sk: *mut u8,
+    seed: *const u8,
+) -> i32 {
+    crypto_sign_seed_keypair(pk, sk, seed)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn self_randombytes_buf(buf: *mut c_void, size: size_t) {
     randombytes_buf(buf, size)
 }
